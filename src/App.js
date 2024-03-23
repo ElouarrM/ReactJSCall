03d338c414f66662ac279b274ba9d233cfef1f43
Monir EL OUARROUDI <info.elouarroudi@gmail.com>
2024-03-23 01:55:02 +0000

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import UploadPDF from './components/UploadPDF';
+
+function App() {
+  return (
+    <div>
+      <h1>PDF Text Extraction App</h1>
+      <UploadPDF />
+    </div>
+  );
+}
+
+export default App;
